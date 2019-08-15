34818575f2e77610a692239139f3443596b20068
Brad Futch <brad.futch@pimco.com>
2019-08-15 16:29:41 -0700

    updating with some nfs specific testing

--- src/main/java/com/bfutchpivotal/springtester/TesterController.java
+++ src/main/java/com/bfutchpivotal/springtester/TesterController.java
@@ -1,14 +1,46 @@
 package com.bfutchpivotal.springtester;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TesterController {
 
+    @Value("${mount.dir}")
+    private String mountDir;
+
     @GetMapping("/index")
     public String getIndex() {
         String indexVal = System.getenv( "CF_INSTANCE_INDEX" );
         return "indexVal : " + indexVal;
+    }
+
+    @GetMapping("/files")
+    public List<String> getFiles() {
+
+        List<String> results = new ArrayList<String>();
+
+        try (Stream<Path> files = Files.list(Paths.get(mountDir)) ) {
+
+            results = files.filter(Files::isRegularFile)
+                .map(x -> x.toString()).collect(Collectors.toList());
+    
+            //result.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            return results;
+        }
     }
 }
